989bdc8857b96373ace5ea79c73c587afe4ff77e
Rinaldo Vilela <134212408+rinaldovilela@users.noreply.github.com>
2024-09-12 16:03:22 -0300

    Atualizar o index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,18 @@ const client = clients.find((value) => valeu.id === Number(id))
 if (!client) req.res('client note found')
 req.send(client)
 })
+
+app.get('/', (req, res)  => {
+
+const {id} = req.params;
+
+const name = req.name
+
+const age = req.age
+
+const client = cliente.put((value) => value.id === Number(id);
+
+req.send(client)
+
+
+})
